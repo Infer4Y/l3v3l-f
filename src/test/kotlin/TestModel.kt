f05aeb651828ddce232b7edcf1b5c8747954b2ee
inferno4you <46350196+inferno4you@users.noreply.github.com>
2019-11-26 21:20:14 -0500

    Cube? Yes, camera is working nicer.

--- src/test/kotlin/TestModel.kt
+++ src/test/kotlin/TestModel.kt
@@ -1,24 +1,51 @@
+import inferno.l3v3l_f.client.models.Model
 import inferno.l3v3l_f.client.models.ModelCube
-import inferno.l3v3l_f.common.utils.math.Vector2f
+import inferno.l3v3l_f.common.utils.math.Matrix3f
 import inferno.l3v3l_f.common.utils.math.Vector3f
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics2D
 import java.util.*
-import javax.swing.InputMap
-import javax.swing.JComponent
 import javax.swing.JFrame
-import javax.swing.KeyStroke
+import javax.swing.text.Keymap
 import kotlin.concurrent.schedule
-import kotlin.math.ceil
-import kotlin.math.floor
+import kotlin.math.*
 
 class TestModel : JFrame("Test Model") {
-    var model = ModelCube(0.toFloat(),0.toFloat(),0.toFloat(), 50f)
-    var camara = Vector3f((50).toFloat(), (50).toFloat(),(-250).toFloat())
 
+    var model = ModelCube(0.toFloat(),0.toFloat(),0.toFloat(), 50f)
+    var model1 = ModelCube(50.toFloat(),0.toFloat(),0.toFloat(), 50f)
+    var model2 = ModelCube(0.toFloat(),50.toFloat(),0.toFloat(), 50f)
+    var model3 = ModelCube(0.toFloat(),0.toFloat(),50.toFloat(), 50f)
+
+    var shapes : Array<Model> = arrayOf(model, model1, model2, model3)
+
+    var west = 100.toFloat()
+    var east = 0.toFloat()
+    var north = 0.toFloat()
+    var south = 100.toFloat()
+
+    var r = 180.toFloat()
+    var q = 90.toFloat()
+
+    var camerax = Matrix3f(
+        floatArrayOf(
+            cos(r), 0.toFloat(), -sin(r),
+            0.toFloat(), 1.toFloat(), 0.toFloat(),
+            sin(r), 0.toFloat(), cos(r)
+        ))
+    var cameray = Matrix3f(
+        floatArrayOf(
+            1.toFloat(), 0.toFloat(), 0.toFloat(),
+            0.toFloat(), cos(q), sin(q),
+            0.toFloat(), -sin(q), cos(q)
+        ))
+
+    var camera = camerax.multiply(cameray)
+    var transformLoc = Vector3f(0.toFloat(),0.toFloat(),100.toFloat())
 
     init {
+
         size = Dimension(300,300)
         isResizable = true
         isVisible = true
@@ -26,42 +53,50 @@ class TestModel : JFrame("Test Model") {
         setLocationRelativeTo(null)
     }
 
+    fun move(){
+        if (west <= 0);
+    }
+
     fun draw() {
-        var last = Vector2f(0f,0f)
+        var last = Vector3f(0.toFloat(),0.toFloat(),0.toFloat())
         val na = last
         var x = 0
-        var current: Vector2f
+        var current: Vector3f
         if (bufferStrategy == null) createBufferStrategy(4)
         val g : Graphics2D = bufferStrategy.drawGraphics as Graphics2D? ?: return
         g.color = Color.BLACK
         g.fillRect(0,0,width, height)
-        g.translate(170,70)
+        g.translate(width/2,height/2)
 
-        for (i in model.vertexes) {
-            current = i.toVector2F(camara)
+        for (l in shapes) {
+            for (i in l.vertexes) {
+                current = camera.transform(i).add(transformLoc)
 
-            if (x == 5) {x =0 ; last = na}
+                if (x == 5) {
+                    x = 0; last = na
+                }
 
-            if (last != na) {
-                g.color = Color.WHITE
+                if (last != na) {
+                    g.color = Color.WHITE
 
-                g.drawLine(
-                    (ceil(last.x)).toInt(),
-                    (floor(last.y)).toInt(),
-                    (ceil(current.x)).toInt(),
-                    (floor(current.y)).toInt()
-                )
+                    g.drawLine(
+                        (floor(last.x)).toInt(),
+                        (floor(last.y)).toInt(),
+                        (floor(current.x)).toInt(),
+                        (floor(current.y)).toInt()
+                    )
+                }
+
+
+                last = current
+                x++
+                g.color = Color.BLACK
             }
-
-            last = current
-            x++
-            g.color = Color.BLACK
         }
 
         //g.translate(0,-800)
         g.dispose()
         bufferStrategy.show()
-        camara.add(Vector3f(0.00f,-0.006f,-0.0f))
     }
 }
 
@@ -80,7 +115,7 @@ object Run{
     }
 
     fun title(){
-        testModel.title = "Test Model | FPS: ${fps}"
+        testModel.title = "Test Model | FPS: $fps"
         fps = 0
         timer1.schedule(1000) {
             title()
@@ -89,7 +124,7 @@ object Run{
 
     @JvmStatic
     fun main(args: Array<String>){
-        timer.schedule(5000) {late()}
+        late()
         title()
     }
 }
